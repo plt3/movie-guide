42bfd301b706cfef9ea9a941358c58d9a619baf3
Paul Taylor <pltorigami@gmail.com>
2023-05-30 13:17:46 -0400

    Add search fields to home page instead of movie list

--- webApp/movieCrud/views.py
+++ webApp/movieCrud/views.py
@@ -2,7 +2,6 @@ from django.core.paginator import EmptyPage, Paginator
 from django.http import Http404
 from django.shortcuts import redirect
 from django.views.generic import DetailView, ListView
-
 from movieCrud.models import Actor, Country, Director, Movie
 
 PER_PAGE = 20
@@ -13,9 +12,32 @@ class HomeListView(ListView):
     template_name = "movieCrud/home.html"
     paginate_by = PER_PAGE
 
+    def getSearchQueryAndType(self):
+        # q and type are submitted when searching via the top search bar
+        searchQuery = self.request.GET.get("q", "")
+        searchType = self.request.GET.get("type", "")
+
+        # all these are submitted only when searching via the home page search fields
+        homeSearchParams = [
+            "qTitle",
+            "qActor",
+            "qDirector",
+            "qCountry",
+            "qYear",
+            "qRating",
+            "qReview",
+        ]
+        for param in homeSearchParams:
+            paramVal = self.request.GET.get(param)
+            if paramVal:
+                searchQuery = paramVal
+                searchType = param[1:].lower()
+                break
+
+        return searchQuery, searchType
+
     def get_queryset(self):
-        searchQuery = self.request.GET.get("q")
-        searchType = self.request.GET.get("type", "title")
+        searchQuery, searchType = self.getSearchQueryAndType()
 
         if searchQuery:
             if searchType == "actor":
@@ -45,14 +67,16 @@ class HomeListView(ListView):
                 return Director.objects.all()
             elif searchType == "country":
                 return Country.objects.all()
-            else:
+            elif searchType == "title":
                 return Movie.objects.all()
+            else:
+                return []
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context["searchQuery"] = self.request.GET.get("q", "")
-        searchType = self.request.GET.get("type", "title")
+        searchQuery, searchType = self.getSearchQueryAndType()
+        context["searchQuery"] = searchQuery
         context["searchType"] = searchType
         context["isMovieObjs"] = searchType in ["title", "year", "rating", "review"]
 
--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret! TODO
-SECRET_KEY = "django-insecure-+^oromv6!)03@o5ofnc$vmv-@-+t+nbxaond=&n-&n$#&9((y5"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production! TODO
-DEBUG = True
+DEBUG = False
 
 # CHANGE THIS LATER TODO
 ALLOWED_HOSTS = ["*"]
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
